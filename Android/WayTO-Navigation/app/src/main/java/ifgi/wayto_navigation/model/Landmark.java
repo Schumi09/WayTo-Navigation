a4a449447676c2e9199fd3c0b325d63012be2ce7
Daniel Schumacher <daniel.schumacher1991@gmail.com>
2016-04-25 17:34:01 +0200

    Use rounded down screen corners for Wedge orientation

--- Android/WayTO-Navigation/app/src/main/java/ifgi/wayto_navigation/model/Landmark.java
+++ Android/WayTO-Navigation/app/src/main/java/ifgi/wayto_navigation/model/Landmark.java
@@ -10,7 +10,6 @@ import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
 import android.location.Location;
 import android.os.AsyncTask;
-import android.util.Log;
 
 import com.google.maps.android.SphericalUtil;
 import com.mapbox.mapboxsdk.annotations.Annotation;
@@ -20,6 +19,7 @@ import com.mapbox.mapboxsdk.annotations.Marker;
 import com.mapbox.mapboxsdk.annotations.MarkerOptions;
 import com.mapbox.mapboxsdk.annotations.PolygonOptions;
 import com.mapbox.mapboxsdk.annotations.PolylineOptions;
+import com.mapbox.mapboxsdk.constants.MathConstants;
 import com.mapbox.mapboxsdk.geometry.LatLng;
 import com.mapbox.mapboxsdk.geometry.VisibleRegion;
 import com.mapbox.mapboxsdk.maps.MapboxMap;
@@ -32,8 +32,11 @@ import com.vividsolutions.jts.geom.Point;
 import com.vividsolutions.jts.geom.Polygon;
 import com.vividsolutions.jts.geom.PrecisionModel;
 import com.vividsolutions.jts.operation.distance.DistanceOp;
+import com.vividsolutions.jts.operation.linemerge.LineMerger;
+import com.vividsolutions.jts.util.GeometricShapeFactory;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 import ifgi.wayto_navigation.R;
@@ -201,32 +204,56 @@ public class Landmark {
                     .icon(this.landmark.getOff_screen_icon())));
         }
 
-        private Polygon wedgeBboxPolygon(Coordinate[] coordinates) {
-            double LONG_OFFSET = coordinates[1].x * WEDGE_CORNER_RATIO  * 0.25;
-            double SHORT_OFFSET = coordinates[2].y * WEDGE_CORNER_RATIO;
-
-            Coordinate[] new_coordinates = new Coordinate[9];
-            new_coordinates[0] = coordinates[0];
-            new_coordinates[0].x += LONG_OFFSET;
-            new_coordinates[1] = coordinates[1];
-            new_coordinates[1].x -= LONG_OFFSET;
-            new_coordinates[2] = coordinates[1];
-            new_coordinates[2].y += SHORT_OFFSET;
-            new_coordinates[3] = coordinates[2];
-            new_coordinates[3].y -= SHORT_OFFSET;
-            new_coordinates[4] = coordinates[2];
-            new_coordinates[4].x -= LONG_OFFSET;
-            new_coordinates[5] = coordinates[3];
-            new_coordinates[5].x += LONG_OFFSET;
-            new_coordinates[6] = coordinates[3];
-            new_coordinates[6].y -= SHORT_OFFSET;
-            new_coordinates[7] = coordinates[3];
-            new_coordinates[7].y += SHORT_OFFSET;
-            new_coordinates[8] = new_coordinates[0];
-
-            return new GeometryFactory().createPolygon(new_coordinates);
+        private Coordinate transform_coordinate(Coordinate coordinate) {
+            coordinate.y = coordinate.y * -1;
+            return coordinate;
         }
 
+        private Coordinate[] transform_coordinates (Coordinate[] coordinates) {
+            for (int i=0; i<coordinates.length; i++) {
+                coordinates[i] = transform_coordinate(coordinates[i]);
+            }
+            return coordinates;
+        }
+
+        private LineString createArc(Coordinate center, double radius, double start_angle, double end_angle) {
+            center = transform_coordinate(center);
+            GeometricShapeFactory gsf = new GeometricShapeFactory();
+            gsf.setSize(radius * 2); // radius
+            gsf.setNumPoints(50);
+            gsf.setCentre(center);
+            LineString coordinates = gsf.createArc(start_angle, end_angle);
+            coordinates = new GeometryFactory().createLineString(transform_coordinates(coordinates.getCoordinates()));
+            return coordinates;
+        }
+
+
+        private Polygon wedgeBboxPolygon(Coordinate[] coordinates) {
+            LineMerger lm = new LineMerger();
+
+            double min_x = coordinates[0].x;
+            double max_x = coordinates[1].x;
+            double min_y = coordinates[0].y;
+            double max_y = coordinates[2].y;
+            double OFFSET = max_y * 1/3;
+
+
+            LineString arc_top_right = createArc(new Coordinate(max_x - OFFSET, min_y + OFFSET), OFFSET, 0, Math.PI /2);
+            LineString arc_bottom_right = createArc(new Coordinate(max_x - OFFSET, max_y - OFFSET), OFFSET, (Math.PI * 1.5), Math.PI /2);
+            LineString arc_bottom_left = createArc(new Coordinate(min_x + OFFSET, max_y - OFFSET), OFFSET, (Math.PI), Math.PI /2);
+            LineString arc_top_left = createArc(new Coordinate(min_x + OFFSET, min_y + OFFSET), OFFSET, (Math.PI / 2), Math.PI /2);
+            lm.add(arc_top_left);
+            lm.add(arc_bottom_left);
+            lm.add(arc_bottom_right);
+            lm.add(arc_top_right);
+            //lm.add(connection);
+            //Coordinate[] lol = arc_top_left_start + arc_top_left_start;
+            Collection ls = lm.getMergedLineStrings();
+            ls.add(new GeometryFactory().createPoint(arc_top_left.getCoordinateN(0)));
+            Geometry geom = new GeometryFactory().buildGeometry(ls);
+
+            return new GeometryFactory().createPolygon(geom.getCoordinates());
+        }
 
         private LatLng calculateMidPoint(LatLng p1, LatLng p2){
             double heading = heading(p1, p2);
@@ -247,11 +274,11 @@ public class Landmark {
         private double calculateLeg(double distanceToScreen) {
             double INTRUSION_CONSTANT = 20;
             double leg = distanceToScreen + Math.log((distanceToScreen + INTRUSION_CONSTANT) / 12) * 10;
-            return leg;
+            return leg + 40;
         }
 
         private double calculateAperture(double dist, double leg) {
-            return Math.toDegrees((5 + dist * 0.1) / leg);
+            return Math.toDegrees((5 + dist * 0.05) / leg);
         }
     }
 
